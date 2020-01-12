605ebfc3169cc103d4449aefdd24d9aeaebf9b88
Matt Wright <mttwright@gmail.com>
2020-01-12 22:21:31 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,10 +6,10 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.integer :employees
-      t.boolean :open
-      t.string :opening_time
-      t.string :closing_time
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
       t.timestamps
     end
   end
